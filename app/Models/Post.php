96c61cb35f8f2cfdd35b764e9c85d090d8b3b38f
RamazanIrm <irmagaliev2001@gmail.com>
2021-03-01 17:34:19 +0500

    create Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class client extends Model
+class Post extends Model
 {
     use HasFactory;
-    protected $table= "client";
+
+    protected $table = "post";
 }
